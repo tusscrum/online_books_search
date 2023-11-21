98c4a3cf1b3c3f2cdc7ac196cb2677cd704e3c9d
zhenyili <lizhenyi1314>
2023-11-21 15:29:41 +0000

    test enviroment

---
+++ conf.py
@@ -0,0 +1,24 @@
+"""
+@author:liazylee
+@license: Apache Licence
+@time: 13/11/2023 20:44
+@contact: li233111@gmail.com
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+              ┏┓      ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃      ☃      ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃      ┻      ┃
+            ┗━┓      ┏━┛
+                ┃      ┗━━━┓
+                ┃  神兽保佑    ┣┓
+                ┃　永无BUG！   ┏┛
+                ┗┓┓┏━┳┓┏┛
+                  ┃┫┫  ┃┫┫
+                  ┗┻┛  ┗┻┛
+"""
+import os
+MOGODB_URL = os.getenv('MONGODB_URL')
+GOOGLE_BOOKS_API_KEY = os.getenv('GOOGLE_BOOKS_API_KEY')
+
