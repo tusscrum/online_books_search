08cfdd400e5fba348ae4681465ed06bbb859cef3
zhenyili <lizhenyi1314>
2023-11-25 21:28:14 +0000

    sort bug

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ async def get_books_list(parqms: Params = Depends()):
     Get books list
     :return: Page [Books]
     """
-    books_list = await books_collection.find().sort({}).to_list(100)
+    books_list = await books_collection.find().sort().to_list(100)
     return paginate(books_list, parqms)
 
 
