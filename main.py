3606d41e4ff4b5e3be44120f4ed3c65f8c47443d
zhenyili <lizhenyi1314>
2023-11-25 23:34:56 +0000

    remove some packages

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # create a database to monogoDB
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Body
+from fastapi.encoders import jsonable_encoder
 from fastapi_pagination import Page, Params, paginate
 from starlette import status
 
-from dbs import books_collection
+from dbs import books_collection, users_books_collection, users_collection, add_or_update_users_books, add_user
 
 # disable_installed_extensions_check()
 
@@ -12,8 +13,8 @@ try:
     from conf import MONGODB_URL
 except:
     pass
-from control import get_book_info, search_books
-from models import Books
+from control import get_book_info, search_books, helper_user
+from models import Books, UserBooks, UpdateUserBooks, UserRegister, UserLogin
 
 app = FastAPI(
     title="Bookstore",
@@ -45,60 +46,65 @@ async def get_books_list(parqms: Params = Depends()):
     return paginate(books_list, parqms)
 
 
-# @app.post('/api/login',
-#           status_code=status.HTTP_200_OK,
-#           response_description='Login',
-#           )
-# async def login(user: UserLogin = Body(...)):
-#     """
-#     Login
-#     :return: User
-#     """
-#     # if no user, return 404
-#
-#     finded_user = await users_collection.find_one({"username": user.username}) or users_collection.find_one(
-#         {"email": user.email}) or None
-#     if not finded_user:
-#         return {"message": "User not found"}, status.HTTP_404_NOT_FOUND
-#
-#     if finded_user.get('password') == user.password:
-#
-#         # set session
-#         finded_user = helper_user(finded_user)
-#         return {"message": "Login success", 'user': finded_user}
-#     else:
-#         return {"message": "Login failed"}  # , status.HTTP_404_NOT_FOUND
-#
-#
-# @app.post('/api/register',
-#
-#           response_description='Register',
-#
-#           )
-# async def register(user: UserRegister = Body(...)):
-#     """
-#     Register
-#     :return: User
-#     """
-#     # if no user, return 404
-#     exit_user = await users_collection.find_one({"username": user.username}) or \
-#                 await users_collection.find_one({"email": user.email}) or None
-#     if exit_user:
-#         return {"message": "User already exists"}, status.HTTP_404_NOT_FOUND
-#     else:
-#         user = jsonable_encoder(user)
-#         user = await add_user(user)
-#         return {"message": "Register success", 'user': user}, status.HTTP_201_CREATED
-#
-#
-# @app.get('/api/user_list', )
-# async def get_user_list(parqms: Params = Depends()):
-#     """
-#     Get user list
-#     :return: Page [User]
-#     """
-#     users_list = await users_collection.find({}).to_list(100)
-#     return paginate(users_list, parqms)
+@app.post('/api/login',
+          status_code=status.HTTP_200_OK,
+          response_description='Login',
+          )
+async def login(user: UserLogin = Body(...)):
+    """
+    Login
+    :return: User
+    """
+    # if no user, return 404
+
+    finded_user = await users_collection.find_one({"username": user.username}) or users_collection.find_one(
+        {"email": user.email}) or None
+    if not finded_user:
+        return {"message": "User not found"}, status.HTTP_404_NOT_FOUND
+
+    if finded_user.get('password') == user.password:
+
+        # set session
+        finded_user = helper_user(finded_user)
+        return {"message": "Login success", 'user': finded_user}
+    else:
+        return {"message": "Login failed"}  # , status.HTTP_404_NOT_FOUND
+
+
+@app.post('/api/register',
+
+          response_description='Register',
+
+          )
+async def register(user: UserRegister = Body(...)):
+    """
+    Register
+    :return: User
+    """
+    # if no user, return 404
+    exit_user = await users_collection.find_one({"username": user.username}) or \
+                await users_collection.find_one({"email": user.email}) or None
+    if exit_user:
+        return {"message": "User already exists"}, status.HTTP_404_NOT_FOUND
+    else:
+        user = jsonable_encoder(user)
+        user = await add_user(user)
+        return {"message": "Register success", 'user': user}, status.HTTP_201_CREATED
+
+
+@app.get('/api/user_list',
+         response_model=Page[UserRegister],
+         )
+async def get_user_list(parqms: Params = Depends()):
+    """
+    Get user list
+    :return: Page [User]
+    """
+    users_list = await users_collection.find().to_list(100)
+
+    return paginate(users_list, parqms)
+
+
 #
 
 @app.get('/api/search',
@@ -144,62 +150,62 @@ async def get_book_by_isbn(isbn: str) -> dict:
 
 
 # get user's books list
-# @app.get('/api/user/{userid}/books',
-#          response_model=Page[UserBooks],
-#          response_model_exclude_unset=True,
-#          response_model_exclude_defaults=True,
-#          status_code=status.HTTP_200_OK,
-#          response_description='Get user\'s books list'
-#          )
-# async def get_user_books_list(userid: str, parqms: Params = Depends()):
-#     """
-#     Get user's books list
-#     :return: Page [UserBooks]
-#     """
-#     user_books_list = await users_books_collection.find({"userid": userid}).to_list(100)
-#     return paginate(user_books_list, parqms)
+@app.get('/api/user/{userid}/books',
+         response_model=Page[UserBooks],
+         response_model_exclude_unset=True,
+         response_model_exclude_defaults=True,
+         status_code=status.HTTP_200_OK,
+         response_description='Get user\'s books list'
+         )
+async def get_user_books_list(userid: str, parqms: Params = Depends()):
+    """
+    Get user's books list
+    :return: Page [UserBooks]
+    """
+    user_books_list = await users_books_collection.find({"userid": userid}).to_list(100)
+    return paginate(user_books_list, parqms)
 
 
-# @app.post('/api/user/{userid}/books',
-#           response_model=UserBooks,
-#           status_code=status.HTTP_201_CREATED,
-#           response_description='Add book to user\'s books list and some comments'
-#
-#           )
-# async def add_book_to_user_books_list(userid: str, user_books: UserBooks = Body(...)):
-#     """
-#     Add book to user's books list
-#     :return: UserBooks
-#     """
-#     # if no user, return 404
-#     _ = users_collection.find_one({"_id": userid})
-#     if not _:
-#         return {"message": "User not found"}, status.HTTP_404_NOT_FOUND
-#     else:
-#         users_books = jsonable_encoder(user_books)
-#         user_books = add_or_update_users_books(users_books)
-#         return {"message": "Add book to user's books list success", 'user_books': user_books}
+@app.post('/api/user/{userid}/books',
+          response_model=UserBooks,
+          status_code=status.HTTP_201_CREATED,
+          response_description='Add book to user\'s books list and some comments'
+
+          )
+async def add_book_to_user_books_list(userid: str, user_books: UserBooks = Body(...)):
+    """
+    Add book to user's books list
+    :return: UserBooks
+    """
+    # if no user, return 404
+    _ = users_collection.find_one({"_id": userid})
+    if not _:
+        return {"message": "User not found"}, status.HTTP_404_NOT_FOUND
+    else:
+        users_books = jsonable_encoder(user_books)
+        user_books = add_or_update_users_books(users_books)
+        return {"message": "Add book to user's books list success", 'user_books': user_books}
 
 
-# @app.put('/api/user/{id}/books/',
-#          response_model=UpdateUserBooks,
-#          status_code=status.HTTP_201_CREATED,
-#          response_description='Update book to user\'s books list and some comments'
-#          )
-# async def update_book_to_user_books_list(id: str, user_books: UserBooks = Body(...)):
-#     """
-#     Update book to user's books list
-#     :return: UserBooks
-#     """
-#     # if no user, return 404
-#
-#     _ = users_books_collection.find_one({"_id": id})
-#     if not _:
-#         return {"message": "book not found"}, status.HTTP_404_NOT_FOUND
-#     else:
-#         user_books = jsonable_encoder(user_books)
-#         user_books = add_or_update_users_books(user_books)
-#         return {"message": "Update book to user's books list success", 'user_books': user_books}
+@app.put('/api/user/{id}/books/',
+         response_model=UpdateUserBooks,
+         status_code=status.HTTP_201_CREATED,
+         response_description='Update book to user\'s books list and some comments'
+         )
+async def update_book_to_user_books_list(id: str, user_books: UserBooks = Body(...)):
+    """
+    Update book to user's books list
+    :return: UserBooks
+    """
+    # if no user, return 404
+
+    _ = users_books_collection.find_one({"_id": id})
+    if not _:
+        return {"message": "book not found"}, status.HTTP_404_NOT_FOUND
+    else:
+        user_books = jsonable_encoder(user_books)
+        user_books = add_or_update_users_books(user_books)
+        return {"message": "Update book to user's books list success", 'user_books': user_books}
 
 
 if __name__ == '__main__':
